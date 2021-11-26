0f9bf114cc94a0e14134fa36fbe08857deae3cff
shivam <shivam.kalbande23@gmail.com>
2021-11-26 11:14:29 +0530

    added pattern printing code

--- ssk/src/main/java/com/example/ssk/SskApplication.java
+++ ssk/src/main/java/com/example/ssk/SskApplication.java
@@ -16,6 +16,24 @@ public class SskApplication extends SpringBootServletInitializer {
 		//welcome w = new welcome();
 		//w.welc();
 		System.out.println("Enter your date of birth:");
+		//package testJavaProject;
+		//import java.util.*;
+
+		//public class triangle {
+		//	public static void main(String [] args) {
+				int i=0;
+				for(i=0 ; i<10; i++) {
+					for(int j=i; j>=0; j--) {
+
+						System.out.print("*");
+					}
+					System.out.println(+(i+1)+"\n");
+				}
+			System.out.println("This is pattern Job running on Master from REMOTR Git Repository ssk232017@gmail.com");
+			//}
+
+		//}
+
 	}
 @Override
 protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
