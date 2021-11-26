73191b2d5d107c6195f8f27ace3935477d724ee2
shivam <shivam.kalbande23@gmail.com>
2021-11-26 17:27:17 +0530

    clean maven project

--- ssk/src/main/java/com/example/ssk/SskApplication.java
+++ ssk/src/main/java/com/example/ssk/SskApplication.java
@@ -21,15 +21,15 @@ public class SskApplication extends SpringBootServletInitializer {
 
 		//public class triangle {
 		//	public static void main(String [] args) {
-				int i=0;
-				for(i=0 ; i<10; i++) {
-					for(int j=i; j>=0; j--) {
+	//			int i=0;
+	//			for(i=0 ; i<10; i++) {
+	//				for(int j=i; j>=0; j--) {
 
-						System.out.print("*");
-					}
-					System.out.println(+(i+1)+"\n");
-				}
-			System.out.println("This is pattern Job running on Master from REMOTR Git Repository ssk232017@gmail.com");
+	//					System.out.print("*");
+	//				}
+	//				System.out.println(+(i+1)+"\n");
+	//			}
+	//		System.out.println("This is pattern Job running on Master from REMOTR Git Repository ssk232017@gmail.com");
 			//}
 
 		//}
